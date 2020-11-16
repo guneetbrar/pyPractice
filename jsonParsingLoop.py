cfa63c8f9cbdaa5bc96f354e55314054ed1f8955
Guneet Brar <guneetsb25@gmail.com>
2020-11-16 10:39:13 -0500

    Working with Google Maps API

---
+++ jsonParsingLoop.py
@@ -0,0 +1,19 @@
+import json
+input = '''
+[
+  { "id" : "001",
+    "x" : "2",
+    "name" : "Chuck"
+  } ,
+  { "id" : "009",
+    "x" : "7",
+    "name" : "Brent"
+  }
+]'''
+
+info = json.loads(input)
+print('User count:', len(info))
+for item in info:
+    print('Name', item['name'])
+    print('ID', item['id'])
+    print('Attribute', item['x'])
